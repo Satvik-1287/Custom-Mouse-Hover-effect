7bc0e54d058298b1251b3640e08e7fb4d9906982
Satvik-1287 <143932945+Satvik-1287@users.noreply.github.com>
2023-12-23 20:26:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let div = document.getElementById("move-div");
+let title = document.getElementById("title");
+
+function isTouchDevice() {
+  try {
+    document.createEvent("TouchEvent");
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+const move = (e) => {
+  try {
+    var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
+    var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+  } catch (error) { }
+  div.style.left = x + "px";
+  div.style.top = y + "px";
+  const rect = title.getBoundingClientRect();
+  if (x > rect.left && x < rect.right && y > rect.top && y < rect.bottom) {
+    div.style.width = 5 + "em";
+    div.style.height = 5 + "em";
+  } else {
+    div.style.width = 2 + "em";
+    div.style.height = 2 + "em";
+  }
+};
+
+document.addEventListener("mousemove", (e) => {
+  move(e);
+});
+
+document.addEventListener("touchmove", (e) => {
+  move(e);
+});
